19d92d4f1e1fe464878bf418ef953e11c1a903a3
stilrdim <stilrdim@gmail.com>
2026-07-07 18:04:48 +0100

    Change all foods now have equal chance of being picked
    Previously, strawberry.png strawberry2.png strawberry3.png and lemon.png there would be 75% chance of ending up with a strawberry, due to 3:1. Now, it'll be equal chance without duplicates, so 50-50% chance not leaning towards foods with more alternative images

--- randomFoodsGen.js
+++ randomFoodsGen.js
@@ -43,7 +43,14 @@ function getRandomFood(foodList) {
 function generateList(desiredFoodsAmount) {
   const allImages = fs.readdirSync(FOODS_DIR, { encoding: "utf-8" });
 
-  const allFoods = allImages.map(file => file.replaceAll("_", " ").replace(/\d/g, "").split(".")[0].trim());
+  const allFoods = [...new Set(
+    allImages.map(file =>
+      file.replaceAll("_", " ")
+        .replace(/\d/g, "")
+        .split(".")[0]
+        .trim()
+    )
+  )];
 
   const generatedFoodList = new Set();
 
